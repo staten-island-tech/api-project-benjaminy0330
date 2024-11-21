e78f44ef33448319dc04a570b783d7243c00162b
Benjamin Yee <benyee1209@gmail.com>
2024-11-21 14:00:07 -0500

    asd
    asd

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,2 +1,11 @@
+function greet(name){
+    const greetPromise = new Promise(function (resolve, reject){
+        resolve(`Hello ${name}`);
+    });
+    return greetPromise;
+}
 
-
+const suzie = greet("suzie");
+suzie.then((hello)=>{
+    console.log(hello);
+});
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -11,7 +11,7 @@ export default {
 
 /**@type {import('tailwindcss').Config}*/
 module.exports = {
-  content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./JS/main.js", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
