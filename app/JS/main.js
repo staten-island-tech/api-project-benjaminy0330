ab4efd8c43a6d5eeb32048cd2011ab4bf050cd07
Benjamin <benyee1209@gmail.com>
2024-11-24 20:34:24 -0500

    s
    dd

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,9 +1,2 @@
 import "../css/style.css"
 
-const URL = {"url": "https://random-d.uk/api/images/51.jpg", "message": "Powered by random-d.uk"}
-
-async function getData(URL){
-    const response = await fetch(URL);
-    console.log(response);
-}
-getData();
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,17 +1,11 @@
-import "../css/style.css"
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/**@type {import('tailwindcss').Config}*/
 module.exports = {
-  content: ["./index.html", "./JS/main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./JS/main.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/daisyui/dist/**/*.js", // Fixed typo
+  ],
   theme: {
     extend: {},
   },
